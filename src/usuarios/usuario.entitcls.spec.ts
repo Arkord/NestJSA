d820d5eaca6b700e162818fd353d3a1a335b0525
octavio mejia <arkord1@gmail.com>
2021-03-08 01:09:47 -0600

    typeorm

---
+++ src/usuarios/usuario.entitcls.spec.ts
@@ -0,0 +1,7 @@
+import { UsuarioEntitcls } from './usuario.entitcls';
+
+describe('UsuarioEntitcls', () => {
+  it('should be defined', () => {
+    expect(new UsuarioEntitcls()).toBeDefined();
+  });
+});
